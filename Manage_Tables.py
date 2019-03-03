26b06fbcaf7a1ae62de4bc72a9063bd3d82a618f
illprogramtheshitoutofyou <43383986+illprogramtheshitoutofyou@users.noreply.github.com>
2019-03-04 00:04:48 +0100

    Add files via upload

---
+++ Manage_Tables.py
@@ -0,0 +1,14 @@
+from pack import SQL
+
+def drop_table(conn,coll):
+    query="DROP TABLE {}".format(coll)
+    SQL.sqlexecute(conn,query)
+
+def create_table(conn,coll,columns):
+    #cursor.set_isolation_level(0)
+    print("CREATE TABLE {0}({1})".format(coll, columns))
+    SQL.sqlexecute(conn,"CREATE TABLE {0}({1})".format(coll, columns))
+    # cursor.execute("CREATE TABLE products({})".format(columns))
+    print("Table {} has been created".format(coll))
+    succes = True
+    return succes
