b6a2a4162f5550828d30b719a3a513dedfffde69
illprogramtheshitoutofyou <43383986+illprogramtheshitoutofyou@users.noreply.github.com>
2019-03-06 12:14:35 +0100

    Add files via upload

---
+++ SQL.py
@@ -0,0 +1,12 @@
+import psycopg2
+import random
+
+def connect(database,username,password,host):
+    conn = psycopg2.connect(database=database,user=username,password=password,host=host)
+    conn.set_session(autocommit=True)
+    return conn
+
+def sqlexecute(conn,sql):
+    cursor = conn.cursor()
+    cursor.execute(sql)
+
